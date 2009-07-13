fe4a9ae3f9a6bbcd8d8872f8f48a53582e9b8e78
Mike Keith <iammikesemail@gmail.com>
2009-07-13 18:47:23 -0400

    adding files

---
+++ RangeWalker/src/mikeisgo/RangeWalker/LocSets/EdgeSet.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package mikeisgo.RangeWalker.LocSets;
+
+import android.location.Location;
+
+/**
+ * @author mike k.
+ *
+ */
+public class EdgeSet {
+	
+	public class LocSet {
+		public Location startPt;
+		public Location endPt;
+	}
+}
---
+++ RangeWalker/src/mikeisgo/RangeWalker/LocSets/LocationSetMgr.java
@@ -0,0 +1,17 @@
+package mikeisgo.RangeWalker.LocSets;
+
+import android.location.Location;
+
+public class LocationSetMgr {
+
+	public String getReport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addToCur(Location start, Location mLoc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
