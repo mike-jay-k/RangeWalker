cb5bed3dc71018a26460b0ac3b2625218cf68623
Mike Keith <iammikesemail@gmail.com>
2009-07-13 18:24:02 -0400

    adding edge sets

---
+++ RangeWalker/src/mikeisgo/RangeWalker/EdgeSets.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package mikeisgo.RangeWalker;
+
+import android.location.Location;
+
+/**
+ * @author mike k.
+ *
+ */
+public class EdgeSets {
+	
+	public class LocSet {
+		public Location startPt;
+		public Location endPt;
+	}
+}
